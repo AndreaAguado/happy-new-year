2a87374949bb82d307f3a43d21f3fa2c887d4c52
AndreaAguado <andrea95am@gmail.com>
2021-12-31 18:06:43 +0100

    add loop to audio

--- src/js/main.js
+++ src/js/main.js
@@ -12,52 +12,52 @@ const audio = document.querySelector('.js-audio-container');
 let html = '';
 let audioHtmlTag;
 const incrementAndShowCounter = () => {
-    const time = document.querySelector('.js-time');
-    if (counter < 12) {
-        paintGrape();
-        audioPlayer();
-        counter++;
-        time.innerHTML = counter;
-    }
+  const time = document.querySelector('.js-time');
+  if (counter < 12) {
+    paintGrape();
+    audioPlayer();
+    counter++;
+    time.innerHTML = counter;
+  }
 
-    else {
-        clearTimeout(temp);
-        time.classList.add('hidden');
-        html = `<img class="img js-img hidden" src="./assets/images/uva.png" alt="uva">`;
-        grapesContainer.innerHTML = html;
-        grapesContainer.classList.add('hidden');
-        audioHtmlTag = '';
-        audioHtmlTag += `<audio controls autoplay>
+  else {
+    clearTimeout(temp);
+    time.classList.add('hidden');
+    html = `<img class="img js-img hidden" src="./assets/images/uva.png" alt="uva">`;
+    grapesContainer.innerHTML = html;
+    grapesContainer.classList.add('hidden');
+    audioHtmlTag = '';
+    audioHtmlTag += `<audio controls autoplay loop>
         <source
           src="https://dm0qx8t0i9gc9.cloudfront.net/previews/audio/BsTwCwBHBjzwub4i4/fireworks-show-looping_fJBf2rV__NWM.mp3"
           type="audio/mpeg">
         <p>Your browser doesn't support HTML5 audio</p>
       </audio>`;
-        audio.innerHTML = audioHtmlTag;
-        celebration();
-    }
+    audio.innerHTML = audioHtmlTag;
+    celebration();
+  }
 };
 
 function paintGrape() {
-    if (counter < 12) {
-        html += `<img class="img js-img" src="./assets/images/uva.png" alt="uva">`;
-        grapesContainer.innerHTML = html;
-    }
+  if (counter < 12) {
+    html += `<img class="img js-img" src="./assets/images/uva.png" alt="uva">`;
+    grapesContainer.innerHTML = html;
+  }
 }
 
 function audioPlayer() {
-    audioHtmlTag = '';
-    audioHtmlTag += `<audio controls autoplay>
+  audioHtmlTag = '';
+  audioHtmlTag += `<audio controls autoplay>
     <source
       src="https://dm0qx8t0i9gc9.cloudfront.net/previews/audio/BsTwCwBHBjzwub4i4/church-bell-tolling_Myv9034u_NWM.mp3"
       type="audio/mpeg">
     <p>Your browser doesn't support HTML5 audio</p>
   </audio>`;
-    audio.innerHTML = audioHtmlTag;
+  audio.innerHTML = audioHtmlTag;
 }
 
 function celebration() {
-    page.classList.add('bg');
-    title.classList.remove('hidden');
+  page.classList.add('bg');
+  title.classList.remove('hidden');
 }
 temp = setInterval(incrementAndShowCounter, 3000);
